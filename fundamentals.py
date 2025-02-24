9413fb0f4aa1d5a7581618bfd611332be7d05f59
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-02-24 11:39:37 +0300

    feat: learn variables in python

---
+++ fundamentals.py
@@ -0,0 +1,13 @@
+# VARIABLES
+# With correct convention and primitive types
+
+course_name = 'IT' # string
+course_code = 23456 # integer
+rating = 23.4 # float
+is_published = True # boolean
+
+
+  ## strings
+greeting = 'Hello World'
+print(greeting [4:-4])
+# : sign is used as an equivalent of up to and the - sign is used to serve as a slice method for characters in a string
