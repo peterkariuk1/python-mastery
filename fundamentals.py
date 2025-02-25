ee05b2acb41731f185f2ac467b09b960df3535b9
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-02-25 17:33:14 +0300

    feat: learn loops in python

--- fundamentals.py
+++ fundamentals.py
@@ -40,16 +40,78 @@ print(f"x: {x}, y: {y}")
 temperature = 26
 if temperature > 30:
     # indent always
-  print("It's hot, drink water")
+    print("It's hot, drink water")
 elif temperature == 26:
-  print("Normal temperature")
+    print("Normal temperature")
 else:
-  print("It's cold")
-  
-  #COMPARISON OPERATORS IN PYTHON
-  > -- GREATER THAN
-  < -- LESS THAN
-  <= -- LESS/EQUAL TO
-  >= -- GREATER/EQUAL TO
-  == -- EQUAL TO
-  != -- NOT EQUAL TO
+    print("It's cold")
+
+    # COMPARISON OPERATORS IN PYTHON
+
+    # > -- GREATER THAN
+    # < -- LESS THAN
+    # <= -- LESS/EQUAL TO
+    # >= -- GREATER/EQUAL TO
+    # == -- EQUAL TO
+    # != -- NOT EQUAL TO
+
+   # TERNARY OPERATORS
+
+   # for IF statements
+student_marks = 38
+
+message = 'Good score!' if student_marks >= 60 else "Need for improvement!"
+print(message)
+
+
+# chaining IF statements
+
+if 60 >= student_marks < 100:  # more readable code for if statements
+    print(message, "2")
+
+# LOGICAL operators
+# and -- all logic varibles must be true
+# or -- atleast one logic variable must be true
+# not -- inverses a boolean value if true becomes false
+
+# FOR LOOPS
+
+for number in range(5):
+    print('This will print 5 times:', number + 1)
+
+# FOR ELSE LOOPS
+
+successful = True
+
+for number in range(6):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Not successful")
+
+# NESTED LOOPS
+    # outer loop
+for a in range(5):
+    # inner loop
+    for b in range(3):
+        print(f"({a},{b})")
+
+# WHILE LOOPS
+while True:
+    command = input("Enter text:")
+    print("Echo:", command)
+    if command.lower() == "quit":
+        break
+
+
+count = 0
+for even_number in range(1,25):
+    if even_number % 2 == 0:
+        count += 1
+        print(even_number)
+print(f"We have {count} even numbers")
+
+
+# print("We have 4 even numbers")
